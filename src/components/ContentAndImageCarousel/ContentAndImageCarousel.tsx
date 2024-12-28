7e025b780fe969404f23edc08d6ee90328471a0b
jindal12ketan <jindal.2002ketan@gmail.com>
2024-12-28 23:01:55 +0530

    Add the react-responsive carousel

--- src/components/ContentAndImageCarousel/ContentAndImageCarousel.tsx
+++ src/components/ContentAndImageCarousel/ContentAndImageCarousel.tsx
@@ -1,118 +1,135 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
-import styles from './ContentAndImageCarousel.module.css';
+import React, { useState, useRef } from "react";
+import styles from "./ContentAndImageCarousel.module.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 type CTAButton = {
-	label: string;
-	url: string;
+  label: string;
+  url: string;
 };
 
 type Content = {
-	textContent: string; // Rich text content as HTML string
+  textContent: string; // Rich text content as HTML string
 };
 
 interface ContentAndImageCarouselProps {
-	headerContent: string; // Rich text content as HTML string
-	contents: Content[];
-	backgroundImage: string;
-	ctaButton?: CTAButton;
+  headerContent: string; // Rich text content as HTML string
+  contents: Content[];
+  backgroundImage: string;
+  ctaButton?: CTAButton;
 }
 
 const ContentAndImageCarousel: React.FC<ContentAndImageCarouselProps> = ({
-	headerContent = '',
-	contents = [],
-	backgroundImage = '',
-	ctaButton = {},
+  headerContent = "",
+  contents = [],
+  backgroundImage = "",
+  ctaButton = {},
 }) => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const [prevIndex, setPrevIndex] = useState(0);
-	const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const listCount = 3;
+  const [activeIndex, setActiveIndex] = useState(0);
 
-	const slideCount = Math.ceil(contents.length / 3);
+  const slideCount = Math.ceil(contents.length / listCount);
 
+  if (slideCount === 0) {
+    return <div>No slides available.</div>;
+  }
 
-	const handleDotClick = (index: number) => {
-		setPrevIndex(activeIndex);
-		setActiveIndex(index);
-		if (intervalRef.current) clearInterval(intervalRef.current);
-	};
+  const createCarouselContent = (index: number) => {
+    const listItems = contents.slice(
+      index * listCount,
+      (index + 1) * listCount
+    );
+    return (
+      <div key={index} className={`${styles.contentContainer}`}>
+        {listItems.length > 0 &&
+          listItems.map((item, index) => (
+            <div
+              key={`${activeIndex}-${index}`}
+              className={styles.textContentContainer}
+            >
+              <div className={styles.iconAndText}>
+                <span className={styles.checkIcon}>✔</span>
+                <div
+                  className={styles.textContent}
+                  dangerouslySetInnerHTML={{ __html: item.textContent }}
+                />
+              </div>
+              <div className={styles.divider}></div>
+            </div>
+          ))}
+      </div>
+    );
+  };
 
-	if (slideCount === 0) {
-		return <div>No slides available.</div>;
-	}
-	const listCount = 3;
-	const listItems = contents.slice(activeIndex * listCount, (activeIndex + 1) * listCount);
+  const contentArray = Array.from(
+    { length: slideCount },
+    (_, index) => index
+  ).map(createCarouselContent);
 
-	const isForward =
-		(activeIndex > prevIndex) ||
-		(activeIndex === 0 && prevIndex === slideCount - 1);
+  const updateCurrentSlide = (index: number) => {
+    if (index !== activeIndex) {
+      setActiveIndex(index);
+    }
+  };
 
-	return (
-		<div className={styles.ContentAndImageCarouselContainer}>
-			{backgroundImage && (
-				<div className={styles.imageWrapper}>
-					<img
-						src={backgroundImage}
-						alt="Background Image"
-						className={styles.backgroundImage}
-					/>
-				</div>
-			)}
-			<div className={styles.carouselContainer}>
-				<div className={styles.headerContent}
-					dangerouslySetInnerHTML={{ __html: headerContent }}
-				/>
-				<div
-					key={activeIndex}
-					className={`${styles.contentContainer} ${isForward ? styles.slideInRight : styles.slideInLeft
-						}`}
-				>
-					{listItems.length > 0 && (
-						listItems.map((item, index) => (
-							<div key={`${activeIndex}-${index}`} className={styles.textContentContainer}>
-								<div className={styles.iconAndText}>
-									<span className={styles.checkIcon}>✔</span> {/* Adjust icon styling */}
-									<div
-										className={styles.textContent}
-										dangerouslySetInnerHTML={{ __html: item.textContent }}
-									/>
-								</div>
-								<div className={styles.divider}></div>
-							</div>
-						))
-					)}
-				</div>
-				<div className={styles.navButtonContainer}>
-					{ctaButton && (
-						<div className={styles.ctaButtonWrapper}>
-							<span className={styles.ctaButton}>
-								<a href={ctaButton.url}>
-									{ctaButton.label}
-								</a>
-							</span>
-						</div>
-					)}
-					{slideCount > 1 && (
-						<div className={styles.navDotsContainer}>
-							{Array.from({ length: slideCount }, (_, index) => index + 1).map((_, index) => (
-								<button
-									key={index}
-									onClick={() => handleDotClick(index)}
-									className={
-										index === activeIndex
-											? `${styles.navDot} ${styles.navDotActive}`
-											: styles.navDot
-									}
-									aria-label={`Go to slide ${index + 1}`}
-								/>
-							))}
-						</div>
-					)}
-				</div>
-			</div>
-
-		</div>
-	);
+  return (
+    <div className={styles.ContentAndImageCarouselContainer}>
+      {backgroundImage && (
+        <div className={styles.imageWrapper}>
+          <img
+            src={backgroundImage}
+            alt="Background Image"
+            className={styles.backgroundImage}
+          />
+        </div>
+      )}
+      <div className={styles.carouselContainer}>
+        <div
+          className={styles.headerContent}
+          dangerouslySetInnerHTML={{ __html: headerContent }}
+        />
+        <Carousel
+          emulateTouch={true}
+          swipeable={true}
+          showThumbs={false}
+          showIndicators={false}
+          showStatus={false}
+          showArrows={false}
+          swipeScrollTolerance={50}
+          selectedItem={activeIndex}
+          // onSwipeStart={() => {
+          //   console.log(activeIndex);
+          // }}
+          onChange={updateCurrentSlide}
+        >
+          {contentArray}
+        </Carousel>
+        {ctaButton && (
+          <div className={styles.ctaButtonWrapper}>
+            <span className={styles.ctaButton}>
+              <a href={ctaButton.url}>{ctaButton.label}</a>
+            </span>
+          </div>
+        )}
+        {slideCount > 1 && (
+          <div className={styles.navDotsContainer}>
+            {contentArray.map((_, index: number) => (
+              <button
+                key={index}
+                onClick={() => updateCurrentSlide(index)}
+                className={
+                  index === activeIndex
+                    ? `${styles.navDot} ${styles.navDotActive}`
+                    : styles.navDot
+                }
+                aria-label={`Go to slide ${index + 1}`}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ContentAndImageCarousel;
